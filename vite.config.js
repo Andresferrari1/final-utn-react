b470687aa0fb906cc166d416aacbc4d445ca05fd
Andres Ferrari <andresferrari590@gmail.com>
2025-03-11 10:39:11 -0300

    Configurando el github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://github.com/Andresferrari1/final-utn-react.git/'
+  base: '/final-utn-react/'
+
 })
